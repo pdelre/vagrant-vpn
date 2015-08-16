4c04fc01b2e17197a16b9bf7a232ec0150359296
Paul DelRe <pdelre@gmail.com>
2015-08-16 14:25:50 -0400

    use vagrant-vbguest to manage VBoxGuestAddition versions

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,10 @@
 
 Vagrant.require_version '>= 1.7.0'
 
+unless Vagrant.has_plugin?('vagrant-vbguest')
+  raise 'vagrant-vbguest is not installed!'
+end
+
 Vagrant.configure('2') do |config|
 
   config.vm.box     = 'ubuntu-14-04-mini-amd64'
--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -2,11 +2,3 @@
 
 # light desktop of choice
 apt-get install --no-install-recommends -y lubuntu-desktop
-
-# reinstall VBox Additions
-mkdir /tmp/vbox
-VER=$(cat /home/vagrant/.vbox_version)
-mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox
-sh /tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
-rmdir /tmp/vbox
